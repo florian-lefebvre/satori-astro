c2f954f5a6b54a71ec11efdd3c1d3a1d90c232fd
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-02-05 12:39:08 +0100

    chore: disable dependabot

--- playground/src/pages/test-to-response.png.ts
+++ playground/src/pages/test-to-response.png.ts
@@ -3,7 +3,7 @@ import { satoriAstroOG } from "satori-astro";
 import { html } from "satori-html";
 // import "astro-carton";
 // import { AstroCarton } from "astro-carton/runtime";
-// import OG from "../components/OG.astro";
+import OG from "../components/OG.astro";
 
 export const GET: APIRoute = async () => {
 	const fontFile = await fetch(
@@ -11,26 +11,13 @@ export const GET: APIRoute = async () => {
 	);
 	const fontData: ArrayBuffer = await fontFile.arrayBuffer();
 
-	return await satoriAstroOG({
-		template: html`<div style="display: flex; justify-items: center; align-items: center; background-color: red; font-family: Inter; height: 100%;">
-                            <h1 style="color: blue;">Test toResponse</h1>
-                        </div>`,
-		width: 1920,
-		height: 1080,
-	}).toResponse({
-		satori: {
-			fonts: [
-				{
-					name: "Inter Latin",
-					data: fontData,
-					style: "normal",
-				},
-			],
-		},
-	});
-	// const carton = new AstroCarton();
+	await import("astro-carton")
+	const { AstroCarton } = await import("astro-carton/runtime")
+
 	// return await satoriAstroOG({
-	// 	template: html(await carton.renderToString(OG)),
+	// 	template: html`<div style="display: flex; justify-items: center; align-items: center; background-color: red; font-family: Inter; height: 100%;">
+    //                         <h1 style="color: blue;">Test toResponse</h1>
+    //                     </div>`,
 	// 	width: 1920,
 	// 	height: 1080,
 	// }).toResponse({
@@ -44,4 +31,20 @@ export const GET: APIRoute = async () => {
 	// 		],
 	// 	},
 	// });
+	const carton = new AstroCarton();
+	return await satoriAstroOG({
+		template: html(await carton.renderToString(OG)),
+		width: 1920,
+		height: 1080,
+	}).toResponse({
+		satori: {
+			fonts: [
+				{
+					name: "Inter Latin",
+					data: fontData,
+					style: "normal",
+				},
+			],
+		},
+	});
 };
