032e62895521957d4eb958190b470891ca31baaa
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-08-30 16:43:07 +0200

    feat: resvg wasm (#9)

--- playground/src/pages/test-to-response.png.ts
+++ playground/src/pages/test-to-response.png.ts
@@ -1,9 +1,6 @@
 import type { APIRoute } from "astro";
 import { satoriAstroOG } from "satori-astro";
 import { html } from "satori-html";
-// import "astro-carton";
-// import { AstroCarton } from "astro-carton/runtime";
-import OG from "../components/OG.astro";
 
 export const GET: APIRoute = async () => {
 	const fontFile = await fetch(
@@ -11,29 +8,10 @@ export const GET: APIRoute = async () => {
 	);
 	const fontData: ArrayBuffer = await fontFile.arrayBuffer();
 
-	await import("astro-carton")
-	const { AstroCarton } = await import("astro-carton/runtime")
-
-	// return await satoriAstroOG({
-	// 	template: html`<div style="display: flex; justify-items: center; align-items: center; background-color: red; font-family: Inter; height: 100%;">
-    //                         <h1 style="color: blue;">Test toResponse</h1>
-    //                     </div>`,
-	// 	width: 1920,
-	// 	height: 1080,
-	// }).toResponse({
-	// 	satori: {
-	// 		fonts: [
-	// 			{
-	// 				name: "Inter Latin",
-	// 				data: fontData,
-	// 				style: "normal",
-	// 			},
-	// 		],
-	// 	},
-	// });
-	const carton = new AstroCarton();
 	return await satoriAstroOG({
-		template: html(await carton.renderToString(OG)),
+		template: html`<div style="display: flex; justify-items: center; align-items: center; background-color: red; font-family: Inter; height: 100%;">
+                            <h1 style="color: blue;">Test toResponse</h1>
+                        </div>`,
 		width: 1920,
 		height: 1080,
 	}).toResponse({
