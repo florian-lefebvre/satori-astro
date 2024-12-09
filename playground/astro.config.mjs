28e24bc76e59208ee757d716b81ddb809ff841f6
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-12-09 14:39:19 +0100

    fix: playground (close #13)

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,8 +1,7 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import satoriAstro from "satori-astro";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), satoriAstro()],
+	integrations: [tailwind()],
 });
